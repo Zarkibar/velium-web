387b297c8b9e999e2cb4e2d2c1023dc18f09e224
Zarkibar <69397923+Zarkibar@users.noreply.github.com>
2025-07-30 01:46:33 +0600

    Update socket-manager.js

--- js/socket-manager.js
+++ js/socket-manager.js
@@ -3,7 +3,7 @@ window.SocketManager = {
   connect() {
     if (!this.socket) {
       // this.socket = io("https://3329246d-d81b-4e46-9245-8141cdffb567-00-1y46zj9v09b24.picard.replit.dev");
-      this.socket = io("http://localhost:5000");
+      // this.socket = io("http://localhost:5000");
       this.socket = io("https://velium-server.onrender.com:5000");
     }
     return this.socket;
