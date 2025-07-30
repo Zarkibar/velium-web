b99045403560ca9ea44d68bc6c864fba01b99f3f
Zarkibar <69397923+Zarkibar@users.noreply.github.com>
2025-07-30 13:48:53 +0600

    Update socket-manager.js

--- js/socket-manager.js
+++ js/socket-manager.js
@@ -4,7 +4,7 @@ window.SocketManager = {
     if (!this.socket) {
       // this.socket = io("https://3329246d-d81b-4e46-9245-8141cdffb567-00-1y46zj9v09b24.picard.replit.dev");
       // this.socket = io("http://localhost:5000");
-      this.socket = io("https://velium-server.onrender.com");
+      this.socket = io("https://present-mandy-zarkibar-5eb7dfec.koyeb.app/");
     }
     return this.socket;
   }
